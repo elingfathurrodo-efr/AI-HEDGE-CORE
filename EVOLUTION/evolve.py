94fc46f5cd49da49741b19bb668c4bda62e7dcca
elingfathurrodo-efr <elingfathurrodo@gmail.com>
2026-03-13 02:59:14 +0700

    Update evolve.py

--- EVOLUTION/evolve.py
+++ EVOLUTION/evolve.py
@@ -1,1 +1,24 @@
-print("evolution cycle start")
+import json
+import random
+import os
+
+signal_file = "DATA/signal.json"
+
+symbol = "XAUUSD"
+
+actions = ["BUY","SELL"]
+
+action = random.choice(actions)
+
+signal = {
+    "symbol": symbol,
+    "action": action,
+    "lot": 0.01
+}
+
+os.makedirs("DATA", exist_ok=True)
+
+with open(signal_file,"w") as f:
+    json.dump(signal,f,indent=2)
+
+print("AI generated signal:",signal)
